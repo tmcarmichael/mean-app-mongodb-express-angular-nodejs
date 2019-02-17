f68df7574ef85f58f40a2ca6e4a70e51207d1220
Thomas Carmichael <ThomasCarmichael5@gmail.com>
2019-02-17 15:37:44 -0600

    angular front-end, nodejs server
    angular front-end, nodejs server

--- mean-price-prediction/src/app/posts/post-list/post-list.component.ts
+++ mean-price-prediction/src/app/posts/post-list/post-list.component.ts
@@ -1,14 +1,28 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Subscription } from 'rxjs'
+import { PostsService } from '../posts.service';
+import { Post } from '../post.model';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent{
-  posts = [
-    {title: 'First Post', content: 'This is the first post content'},
-    {title: 'First Post', content: 'This is the second post content'},
-    {title: 'First Post', content: 'This is the third post content'}
-  ];
+export class PostListComponent implements OnInit, OnDestroy{
+  posts: Post[] = [];
+  private postsSub: Subscription;
+
+  constructor(public postsService: PostsService) {}
+
+  ngOnInit(){
+    this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener().subscribe((posts: Post[]) => {
+      this.posts = posts;
+    });
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+  }
+
 }
